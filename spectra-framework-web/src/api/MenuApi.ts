ecc3b7a377464ce89a72a10cef29f279fffa5a99
yangxj96 <yangxj96@gmail.com>
2025-06-04 01:23:45 +0800

    feat: 菜单树形列表,新增/编辑框

--- spectra-framework-web/src/api/MenuApi.ts
+++ spectra-framework-web/src/api/MenuApi.ts
@@ -1,10 +1,6 @@
 import http from "@/plugin/request";
 
 export default {
-    // 分页获取路由列表
-    async page(params?: MenuPageParams): Promise<IResult<Page<Menu>>> {
-        return http.get<IResult<Page<Menu>>>("/api/menu/page", { params }).then(res => res.data);
-    },
     // 获取树形路由
     async tree(): Promise<IResult<Menu[]>> {
         return http.get<IResult<Menu[]>>("/api/menu/tree").then(res => res.data);
