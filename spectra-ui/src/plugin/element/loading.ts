bb49277d783264baa7f9b14395b861bd662dfcd8
yangxj96 <yangxj96@gmail.com>
2025-09-23 15:34:50 +0800

    style: style

--- spectra-ui/src/plugin/element/loading.ts
+++ spectra-ui/src/plugin/element/loading.ts
@@ -1,19 +1,3 @@
-/*
- *  Copyright 2018-2025 yangxj96
- *
- *  Licensed under the Apache License, Version 2.0 (the "License");
- *  you may not use this file except in compliance with the License.
- *  You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- *  Unless required by applicable law or agreed to in writing, software
- *  distributed under the License is distributed on an "AS IS" BASIS,
- *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *  See the License for the specific language governing permissions and
- *  limitations under the License.
- */
-
 // ElLoading       : loading组件
 // ILoadingInstance: loading对象类型接口
 import _ from "lodash";
@@ -30,7 +14,7 @@ let startTime: number = 0;
 const minLoadingTime: number = 300;
 
 // loading对象
-let loading: LoadingInstance;
+let loading: LoadingInstance | undefined;
 
 function open(): void {
     startTime = Date.now();
@@ -44,7 +28,7 @@ function open(): void {
     });
 }
 
-async function close(): void {
+async function close(): Promise<void> {
     // 计算是否需要补足最小 loading 时间
     const elapsed = Date.now() - startTime;
     const remaining = Math.max(0, minLoadingTime - elapsed);
