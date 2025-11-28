48199321d288af9b6283a70d75bd7a9109e75b4e
yangxj96 <yangxj96@gmail.com>
2025-11-28 15:04:57 +0800

    perf: 还原数据库监控页面,找到一个DRUID的快照版暂时顶替使用

--- spectra-ui/src/views/Monitor/Database/index.vue
+++ spectra-ui/src/views/Monitor/Database/index.vue
@@ -1,24 +1,7 @@
 <script setup lang="ts">
-import DatabaseApi from "@/api/DatabaseApi.ts";
-import { type ParsedMetrics, parseHikariCpMetrics } from "@/utils/HikariCpMetrics.ts";
-import { ref } from "vue";
-
-const m = ref<ParsedMetrics>();
-
-try {
-    const monitors = await DatabaseApi.getDatabaseMonitors();
-    m.value = parseHikariCpMetrics(monitors.data);
-} catch (e) {
-    console.error(e);
-}
-
+const url = import.meta.env.VITE_API_URL + "api/druid/index.html?t_" + Date.now();
 </script>
 
 <template>
-    <div>
-        {{ m?.summary.healthStatus + "____" + m?.summary.message}}
-        <p v-if="m" v-for="(item,idx) in m?.pools" :key="idx">
-            {{ JSON.stringify(item)}}
-        </p>
-    </div>
+    <iframe :src="url" title="Druid监控页面" style="padding: 0; border: 0; height: 99%; width: 100%" />
 </template>
