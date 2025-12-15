6633d3555d8561df6d0d49541d52eac2179aa4b6
yangxj96 <yangxj96@gmail.com>
2025-12-15 15:09:15 +0800

    feat: Security持久化到Redis的一些问题

--- spectra-admin/spectra-core/src/main/java/io/github/yangxj96/spectra/core/configure/redis/RedisTemplateFactory.java
+++ spectra-admin/spectra-core/src/main/java/io/github/yangxj96/spectra/core/configure/redis/RedisTemplateFactory.java
@@ -1,7 +1,6 @@
 package io.github.yangxj96.spectra.core.configure.redis;
 
 
-import org.jspecify.annotations.NullUnmarked;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.JacksonJsonRedisSerializer;
@@ -17,7 +16,9 @@ import tools.jackson.databind.ObjectMapper;
  */
 public abstract class RedisTemplateFactory {
 
-    @NullUnmarked
+    private RedisTemplateFactory() {
+    }
+
     public static RedisTemplate<String, Object> build(RedisConnectionFactory factory, ObjectMapper om) {
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(factory);
