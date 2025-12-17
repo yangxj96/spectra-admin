a0690ce6bcab97670377ae454434d51eed57850b
yangxj96 <yangxj96@gmail.com>
2025-12-17 16:27:08 +0800

    feat: 流程定义模块的内容,在扩展LogicFlow包实现Flowable的规范

--- spectra-ui/src/views/System/Workflow/components/EditWorkflow/index.vue
+++ spectra-ui/src/views/System/Workflow/components/EditWorkflow/index.vue
@@ -8,10 +8,14 @@ import "@logicflow/extension/dist/index.css";
 
 const container = useTemplateRef<HTMLDivElement>("container");
 
+const panel = useTemplateRef<HTMLDivElement>("panel");
+
+let logicFlow: LogicFlow;
+
 onMounted(() => {
     if (!container.value) return;
 
-    const lf = new LogicFlow({
+    logicFlow = new LogicFlow({
         container: container.value!,
         grid: true,
         plugins: [Control, DndPanel, SelectionSelect, FlowablePlugin],
@@ -22,53 +26,37 @@ onMounted(() => {
         }
     });
 
-    lf.getGraphData();
+    logicFlow.getGraphData();
 
-    (lf.extension.dndPanel as DndPanel)?.setPatternItems(Flowable.getFlowableDndItems());
-
-    lf.render({
-        nodes: [
-            {
-                id: "n1",
-                type: "bpmn:startEvent",
-                x: 210,
-                y: 110
-            },
-            {
-                id: "n2",
-                type: "bpmn:userTask",
-                x: 410,
-                y: 110
-            },
-            {
-                id: "n3",
-                type: "bpmn:endEvent",
-                x: 610,
-                y: 110
-            }
-        ],
-        edges: [
-            {
-                id: "sf1",
-                type: "bpmn:sequenceFlow",
-                sourceNodeId: "n1",
-                targetNodeId: "n2"
-            },
-            {
-                id: "sf2",
-                type: "bpmn:sequenceFlow",
-                sourceNodeId: "n2",
-                targetNodeId: "n3"
-            }
-        ]
+    (logicFlow.extension.dndPanel as DndPanel)?.setPatternItems(Flowable.getFlowableDndItems());
+    (logicFlow.extension.control as Control)?.addItem({
+        key: "export",
+        title: "",
+        text: "导出",
+        iconClass: "export",
+        onClick: lf => {
+            console.log(lf);
+            let xml = Flowable.toBpmnXml(lf);
+            console.log(xml);
+        }
     });
 
-    console.log(Flowable.toBpmnXml(lf.getGraphData()));
+    logicFlow.render({});
+
+    Flowable.registerPropertyPanel({
+        container: panel.value!,
+        lf: logicFlow
+    });
 });
 </script>
 
 <template>
     <el-row style="height: 100%">
-        <div ref="container" style="height: 100%; width: 100%" />
+        <el-col :span="18" style="height: 100%">
+            <div ref="container" style="height: 100%; width: 100%" />
+        </el-col>
+        <el-col :span="6" style="height: 100%">
+            <div ref="panel" style="height: 100%; width: 100%" />
+        </el-col>
     </el-row>
 </template>
