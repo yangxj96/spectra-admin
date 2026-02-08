ad0b5262e691f6758c00ec2ff7f52d98bff36881
yangxj96 <yangxj96@gmail.com>
2026-02-09 00:24:27 +0800

    docs: 模块开始多了,归类下

---
+++ spectra-admin/spectra-starter/spectra-security-web-starter/src/main/java/io/github/yangxj96/spectra/security/web/WebSecurityAutoConfiguration.java
@@ -0,0 +1,9 @@
+package io.github.yangxj96.spectra.security.web;
+
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class WebSecurityAutoConfiguration {
+
+
+}
