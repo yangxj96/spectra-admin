14cc05d46c6a8e858ffd9321dc2ebe3239d35d88
yangxj96 <yangxj96@gmail.com>
2026-03-02 00:42:36 +0800

    perf: 准备把数据范围相关功能移动到单独的包中

---
+++ spectra-admin/spectra-starter/spectra-datascope-base/src/main/java/io/github/yangxj96/spectra/datascope/base/annotation/DataScope.java
@@ -0,0 +1,9 @@
+package io.github.yangxj96.spectra.datascope.base.annotation;
+
+/// 数据范围
+///
+/// @author Jack Young
+/// @version 1.0
+/// @since 2026/2/28 17:59
+public @interface DataScope {
+}
---
+++ spectra-admin/spectra-starter/spectra-datascope-base/src/main/java/io/github/yangxj96/spectra/datascope/base/aspect/DataScopeAspect.java
@@ -0,0 +1,12 @@
+package io.github.yangxj96.spectra.datascope.base.aspect;
+
+
+/**
+ * 数据范围切面处理
+ *
+ * @author Jack Young
+ * @version 1.0
+ * @since 2026/2/28 18:00
+ */
+public class DataScopeAspect {
+}
