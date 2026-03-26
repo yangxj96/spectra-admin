a8853c83d506a3b577d639ecc44c6627d849f83d
yangxj96 <yangxj96@gmail.com>
2026-03-26 18:08:38 +0800

    build: 单元测试和web依赖升级成spring boot 4.x新拆分的webmvc

---
+++ spectra-admin/spectra-modules/spectra-core/src/test/java/com/devops00/spectra/core/service/IpLocationServiceTest.java
@@ -0,0 +1,31 @@
+package com.devops00.spectra.core.service;
+
+
+import com.devops00.spectra.core.service.common.IpLocationService;
+import jakarta.annotation.Resource;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/// IP转地址测试
+///
+/// @author Jack Young
+/// @version 1.0
+/// @since 2026/3/26 17:56
+@Slf4j
+@SpringBootTest
+public class IpLocationServiceTest {
+
+    @Resource
+    private IpLocationService ipLocationService;
+
+    /**
+     * 测试IP转区域效果
+     */
+    @Test
+    void testIpCheck() {
+        var region = ipLocationService.getCityEn("106.60.114.81", 3);
+        log.atInfo().log("当前地址:{}", region);
+    }
+
+}
