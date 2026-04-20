4a30d20ce1c39d7e3bbff742e256754aaa4155ca
yangxj96 <yangxj96@gmail.com>
2026-04-20 11:58:38 +0800

    docs: 补充注释

--- spectra-admin/spectra-common/src/main/java/com/devops00/spectra/common/assembler/converter/IdConverter.java
+++ spectra-admin/spectra-common/src/main/java/com/devops00/spectra/common/assembler/converter/IdConverter.java
@@ -7,7 +7,13 @@ package com.devops00.spectra.common.assembler.converter;
 /// @since 2026/4/20 11:41
 public interface IdConverter<ID> {
 
+    /// 转换到字符串
+    ///
+    /// @param id 值
     String toString(ID id);
 
+    /// 转换到指定类型
+    ///
+    /// @param value 值
     ID fromString(String value);
 }
