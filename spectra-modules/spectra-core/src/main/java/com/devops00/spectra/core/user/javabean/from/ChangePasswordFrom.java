0827a3262579e6dd3968faa11cfd2cf14e3d3a29
yangxj96 <yangxj96@gmail.com>
2026-07-19 04:55:09 +0800

    feat(user): 新增修改密码入参对象 ChangePasswordFrom

---
+++ spectra-modules/spectra-core/src/main/java/com/devops00/spectra/core/user/javabean/from/ChangePasswordFrom.java
@@ -0,0 +1,52 @@
+/*
+ *  Copyright 2018-2026 yangxj96
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.devops00.spectra.core.user.javabean.from;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/// 修改密码入参
+///
+/// @author yangxj96
+/// @version 1.0
+/// @since 2026/7/19
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ChangePasswordFrom {
+
+    /// 旧密码
+    @NotBlank(message = "旧密码不能为空")
+    private String old_password;
+
+    /// 新密码
+    @NotBlank(message = "新密码不能为空")
+    @Size(min = 6, max = 20, message = "密码长度必须在6-20位之间")
+    @Pattern(
+        regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{6,20}$",
+        message = "密码必须包含大小写字母、数字和特殊字符（@$!%*?&）"
+    )
+    private String new_password;
+
+    /// 确认密码
+    @NotBlank(message = "确认密码不能为空")
+    private String verify_password;
+}
