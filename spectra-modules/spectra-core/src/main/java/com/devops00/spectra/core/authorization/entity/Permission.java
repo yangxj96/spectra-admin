e63cf8f3d7b69d802dbee989e1bcd9d1d6f31cf6
yangxj96 <yangxj96@gmail.com>
2026-08-14 17:42:27 +0800

    feat(security): 接入数据库授权快照加载器

---
+++ spectra-modules/spectra-core/src/main/java/com/devops00/spectra/core/authorization/entity/Permission.java
@@ -0,0 +1,42 @@
+/*
+ *  Copyright 2018-2026 yangxj96
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.devops00.spectra.core.authorization.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+
+import java.util.UUID;
+
+@Data
+@TableName(value = "permission", schema = "spectra_security")
+public class Permission {
+
+    @TableId(value = "id", type = IdType.INPUT)
+    private UUID id;
+
+    @TableField(value = "code")
+    private String code;
+
+    @TableField(value = "name")
+    private String name;
+
+    @TableField(value = "state")
+    private String state;
+}
---
+++ spectra-modules/spectra-core/src/main/java/com/devops00/spectra/core/authorization/mapper/AuthorizationScopeMapper.java
@@ -0,0 +1,21 @@
+/*
+ *  Copyright 2018-2026 yangxj96
+ *
+ *  Licensed under the Apache License, Version 2.0 (the "License");
+ *  you may not use this file except in compliance with the License.
+ *  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+package com.devops00.spectra.core.authorization.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.devops00.spectra.core.authorization.entity.AuthorizationScope;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface AuthorizationScopeMapper extends BaseMapper<AuthorizationScope> {
+}
